d7affcecd09c409e01b2824981da720f5cdb3127
cyan <97122638+cyan-oj@users.noreply.github.com>
2023-01-13 20:31:24 -0500

    basic draw call

--- draw.js
+++ draw.js
@@ -1,7 +1,26 @@
+const VSHADER_SOURCE = `
+  void main() {
+    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+    gl_PointSize = 10.0;
+  }
+`
+
+const FSHADER_SOURCE = `
+  void main() {
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  }
+`
+
 function main() { 
   const canvas = document.getElementById("example");
   const gl = canvas.getContext('webgl');
 
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.error('failed to initialize shaders')
+  }
+
   gl.clearColor(0.0, 0.0, 0.5, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT)
+
+  gl.drawArrays(gl.points, 0, 1)
 }
